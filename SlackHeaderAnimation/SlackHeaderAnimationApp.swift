aebb17e13c1874eecc5a84fe942a9aa44fc576b9
superkookai <weerawut@live.com>
2025-04-15 12:37:32 +0700

    Initial Commit

---
+++ SlackHeaderAnimation/SlackHeaderAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlackHeaderAnimationApp.swift
+//  SlackHeaderAnimation
+//
+//  Created by Weerawut Chaiyasomboon on 15/04/2568.
+//
+
+import SwiftUI
+
+@main
+struct SlackHeaderAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
